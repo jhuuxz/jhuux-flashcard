8b55cc8ffed4f141289d79cb0b615b96918fafb4
jhuuxz <jhulien.ferreira@escola.pr.gov.br>
2025-07-04 08:10:42 -0300

    1.2

---
+++ assets/perguntas.js
@@ -0,0 +1,63 @@
+criaCartao(
+    'história',
+    'Quais foram as principais causas da segunda guerra mundial?',
+    'As principais causas foram as condições do tratado de Versalhes, a ascensão do nazismo, facismo e militarismo japonês'
+)
+
+
+criaCartao(
+    'História',
+    'Quais países formavam o Eixo na segunda guerra mundial?'
+    'Os países foram: Alemanha, Itália e japão'
+)
+
+
+criaCartao(
+    'História'
+    'Quais países formavam os aliados?',
+    'Os países foram: Estados Unidos, Reino Unido e União Soviética'
+)
+
+
+criaCartao(
+    'História',
+    'Qual foi o estopim da segunda guerra mundial?',
+    'O estopim foi a invasão da Polônia pela Alemanha em 1 de setembro de 1939.'
+)
+
+
+criaCartao(
+    'História',
+    'Qual foi a participação do Brasil na segunda guerra mundial?',
+    'Enviou tropas para lutar ao lado dos aliados na Itália'
+)
+
+
+criaCartao(
+    'História',
+    'O que foi o Holocausto?',
+    'Foi o extermínio de cerca de seis milhões de judeus e outras minorias etnicas e sociais pelos nazistas.'
+)
+
+
+criaCartao(
+    'História',
+    'Como terminou a segunda guerra mundial?',
+    'Alemanha se rendeu em 8 de maio de 1945, e o Japão se rendeu em 2 de setembro de 1945 após os bombardeios atômicos de Hiroshima e Nagasaki.'
+)
+
+
+criaCartao(
+    'História'
+    'Quais foram as consequências da segunda guerra mundial?',
+    'As consequências foram a criação da ONU, a divisão da Alemanha, o início da Guerra Fria e a descolonização de vários países africanos e asiáticos.'
+)
+
+
+criaCartao(
+    'História'
+    'Quanto tempo durou a segunda guerra mundial?',
+    'Durou de 1939 a 1945, totalizando seis anos.'
+)
+
+
