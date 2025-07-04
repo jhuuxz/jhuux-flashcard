40a00e19028195208184f253450c3b33d3df9c47
jhuuxz <jhulien.ferreira@escola.pr.gov.br>
2025-07-04 17:01:18 -0300

    colocar parte do código que faltou

--- app.js
+++ app.js
@@ -21,3 +21,13 @@ let respostaEstaVisivel = false
 
 
 function viraCartao() {
+    respostaEstaVisivel = !respostaEstaVisivel
+    cartao.classList.toggle('active', respostaEstaVisivel)
+}
+cartao.addEventListener('click', viraCartao)
+   
+    container.appendChild(cartao)
+ }
+   
+   
+
